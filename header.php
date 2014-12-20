2befaa034ad3bc3490d815706f3b1c3982e33a55
= <=>
2014-12-19 20:02:39 -0800

    title

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title><?php if(wp_title('', false)) { wp_title('') . ' | '; } echo bloginfo('name'); ?></title>
+        <title><?php wp_title('', true); ?> - <?php echo bloginfo('name'); ?></title>
         <meta name="description" content="<?php bloginfo('description'); ?>">
 
         <link href="//www.google-analytics.com" rel="dns-prefetch">
