4eb5ebc68158378fabe6037688c80c6e850c2240
= <=>
2014-12-24 02:01:30 -0800

    changed share link pop up titles

--- single.php
+++ single.php
@@ -35,10 +35,10 @@
 
 				<div class="pull-left share">
 					<a href="https://twitter.com/intent/tweet?text=<?php echo urlencode(the_title().' - '.get_permalink()); ?>" class="symbol twitter" data-toggle="tooltip" data-placement="bottom" title="Tweet" target="_blank">&#xe086;</a>
-					<a href="https://www.facebook.com/sharer/sharer.php?app_id=351533488351274&sdk=joey&u=<?=urlencode(get_permalink())?>&t=<?=the_title('', '', false)?>&display=popup&ref=plugin" class="symbol facebook" data-toggle="tooltip" data-placement="bottom" title="Share">&#xe027;</a>
-					<a href="https://plus.google.com/share?url=<?=urlencode(get_permalink())?>" class="symbol google" data-toggle="tooltip" data-placement="bottom" title="Share" target="_blank">&#xe039;</a>
-					<a href="//www.reddit.com/submit?url=<?=urlencode(get_permalink())?>" class="symbol reddit" data-toggle="tooltip" data-placement="bottom" title="Share" target="_blank">&#xe069;</a>
-					<a href="https://www.tumblr.com/share/link?url=<?=urlencode(get_permalink())?>&name=<?=urlencode(the_title('', '', false))?>&description=<?=urlencode(the_title('', '', false))?>" class="symbol tumblr" data-toggle="tooltip" data-placement="bottom" title="Share" target="_blank">&#xe085;</a>
+					<a href="https://www.facebook.com/sharer/sharer.php?app_id=351533488351274&sdk=joey&u=<?=urlencode(get_permalink())?>&t=<?=the_title('', '', false)?>&display=popup&ref=plugin" class="symbol facebook" data-toggle="tooltip" data-placement="bottom" title="Share on Facebook">&#xe027;</a>
+					<a href="https://plus.google.com/share?url=<?=urlencode(get_permalink())?>" class="symbol google" data-toggle="tooltip" data-placement="bottom" title="Share on Google +" target="_blank">&#xe039;</a>
+					<a href="//www.reddit.com/submit?url=<?=urlencode(get_permalink())?>" class="symbol reddit" data-toggle="tooltip" data-placement="bottom" title="Post on Reddit" target="_blank">&#xe069;</a>
+					<a href="https://www.tumblr.com/share/link?url=<?=urlencode(get_permalink())?>&name=<?=urlencode(the_title('', '', false))?>&description=<?=urlencode(the_title('', '', false))?>" class="symbol tumblr" data-toggle="tooltip" data-placement="bottom" title="Post on Tumblr" target="_blank">&#xe085;</a>
 					<a href="//pinterest.com/pin/create/button/?url=<?=urlencode(get_permalink())?>&media=<?=$img_url?>&description=<?=urlencode(the_title('', '', false))?>" class="symbol pinterest" data-toggle="tooltip" data-placement="bottom" title="Pin&nbsp;It" target="_blank">&#xe064;</a>
 					</div>
 
