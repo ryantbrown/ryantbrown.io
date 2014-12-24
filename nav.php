56f85af87b1758b890773255bd7b8f7b65aba890
= <=>
2014-12-24 00:36:11 -0800

    overflow

--- nav.php
+++ nav.php
@@ -5,9 +5,10 @@
 </div>
 <nav>
     <ul>
-        <li><a href="/?s=">Blog</a></li>
+<!--        <li><a href="/?s=">Blog</a></li>-->
+        <li><a href="/?s=" class="symbol" target="_blank">&#xe005;</a></li>
         <li><a href="http://twitter.com/r_t_brown" class="symbol" target="_blank">&#xe086;</a></li>
-        <li><a href="http://github.com/ryantbrown" class="symbol" target="_blank">&#xe037;</a></li>
+        <li><a href="http://github.com/ryantbrown" class="symbol" target="_blank">&#xe036;</a></li>
         <li><a href="https://dribbble.com/ryantbrown" class="symbol" target="_blank">&#xe021;</a></li>
         <li><a href="mailto:me@ryantbrown.io" class="symbol">&#xe024;</a></li>
     </ul>
